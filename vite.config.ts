4f68dc030edc6c88067e3ac76e1da8d10f5114f2
Yunkyeong Jang <yunkyeong@dencomm.ai>
2025-02-10 00:24:52 +0900

    [chore]: 모든 포트 허용하게 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // 로컬 IP로 접근할 수 있게 허용
+    host: '0.0.0.0',
     port: 3000, // 필요한 경우 포트를 지정
   },
-})
+});
